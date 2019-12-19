8e563b9cfb343f6989a9b11ae78cfcb49c9eff0c
徐承 <xucheng.pc@alibaba-inc.com>
2019-12-19 12:17:10 +0800

    init

--- src/tapable/index.js
+++ src/tapable/index.js
@@ -1,15 +1,80 @@
-import SyncHook from './SyncHook';
-import SyncBailHook from './SyncBailHook';
-import SyncWaterfallHook from './SyncWaterfallHook';
-import SyncLoopHook from './SyncLoopHook';
-import AsyncParallelHook from './AsyncParallelHook';
-import AsyncSeriesHook from './AsyncSeriesHook';
+export default class Tapable {
+  constructor() {
+    this.tasks = {};
+  }
 
-export default {
-  SyncHook,
-  SyncBailHook,
-  SyncWaterfallHook,
-  SyncLoopHook,
-  AsyncParallelHook,
-  AsyncSeriesHook,
+  tabSync(name, task) {
+    this.tasks[name] = this.tasks[name] || [];
+    this.tasks[name].push(task);
+  }
+
+  callSync(name, ...args) {
+    const tasks = this.tasks[name];
+    for (let i = 0, l = tasks.length; i < l; i++) {
+      tasks[i](...args);
+    }
+  }
+
+  callSyncBail(name, ...args) {
+    let i = 0, ret;
+    do {
+      ret = this.tasks[name][i++](...args);
+    } while (!ret);
+  }
+
+  callLoop(name, ...args) {
+    const tasks = this.tasks[name];
+    for (let i = 0, l = tasks.length; i < l; i++) {
+      let ret;
+      do {
+        ret = tasks[i](...args);
+      } while (ret === true || !(ret === undefined));
+    }
+  }
+
+  callWaterfall(name, ...args) {
+    const tasks = this.tasks[name];
+    return tasks.reduce((ret, task) => task(ret), ...args);
+  }
+
+  callAsyncParallel(name, ...args) {
+    const tasks = this.tasks[name];
+    const finanlCallback = args.pop();
+    let i = 0;
+    const done = () => {
+      if (++i === tasks.length) {
+        finanlCallback()
+      }
+    }
+
+    for(let i = 0, l = tasks.length; i < l; i++) {
+      tasks[i](...args, done);
+    }
+  }
+
+  callPromiseParallel(name, ...args) {
+    const tasks = this.tasks[name];
+    return Promise.all(tasks.map(task => task(...args)));
+  }
+
+  callAsyncSeries(name, ...args) {
+    const tasks = this.tasks[name];
+    const finalCallback = args.pop();
+
+    let i = 0;
+    const next = () => {
+      const task = tasks[i++];
+      task ? task(...args, next) : finalCallback();
+    }
+
+    next();
+  }
+
+  callPromiseSeries(name, ...args) {
+    const tasks = this.tasks[name];
+
+    return tasks.reduce((promise, task) => {
+      return promise.then(() => task(...args));
+    }, Promise.resolve());
+  }
 }
